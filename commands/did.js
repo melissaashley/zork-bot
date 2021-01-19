5d78481e727fe8450554c73ca3d2fe287597eecc
Melissa <melissaashleym@gmail.com>
2021-01-19 00:28:15 -0700

    convert did response into message embed with more data

--- commands/did.js
+++ commands/did.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
 	name: 'dragon',
 	description: 'Return a url to the provided dragon id',
@@ -5,13 +7,24 @@ module.exports = {
 		const baseURL = 'https://www1.flightrising.com/dragon/';
 		const firstID = args[0];
 
+		let parseID = firstID.substring(0, firstID.length - 2);
+		let pID = parseInt(parseID) + 1;
+
+		const embed = new MessageEmbed()
+			.setTitle(`Dragon: #${firstID}`)
+			.setColor(0x20dda4)
+			.attachFiles(`http://flightrising.com/rendern/350/${pID}/${firstID}_350.png`)
+			.setImage(`attachment://${firstID}_350.png`)
+			.setDescription(`${baseURL}${firstID}`);
+
+
 		// re-write, just for testing purposes
 		if (!args.length) {
 			return message.reply('you must provide an id!');
 		} else if (args.length > 1) {
 			return message.reply('OOPS! Looks like you have multiple ids, I only accept one.');
 		} else {
-			return message.reply(`Here you go! \n ${baseURL}${firstID}`);
+			return message.channel.send(embed);
 		}
 	},
 };
