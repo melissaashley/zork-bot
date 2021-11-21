e5efd2a06c6e24ed64857d6b5e64f9be3b66720c
Melissa <melissaashleym@gmail.com>
2021-11-21 12:58:13 -0700

    fix db conversion

--- commands/mg.js
+++ commands/mg.js
@@ -4,16 +4,27 @@ module.exports = {
 	name: 'db',
 	description: 'Return the mg / mmol based on the input `!zork db <mg>` or `!zork db <mmol>`',
 	execute(message, args) {
+
+		const roundNum = function(num, dec) {
+			return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+		};
+
 		const inputNum = args[0];
-		const mg = parseInt(inputNum * 18).toFixed(1);
-		const mmol = parseInt(inputNum / 18).toFixed(1);
+		const mgdlc = 0.05555555555;
+		const mmolc = 18;
 
-		if ( mmol > 0 ) {
-			message.channel.send(`${inputNum} mg/dL is **${mmol} mmol/L** \n`);
+		const mmolVal = (parseFloat(inputNum) + 0) * mgdlc;
+		const mgVal = (parseFloat(inputNum) + 0) * mmolc;
+
+		const mmolFormatted = roundNum(mmolVal, 1);
+		const mgFormatted = roundNum(mgVal, 0);
+
+		if ( mgFormatted <= 900 ) {
+			message.channel.send(`newline: ${inputNum} mmol/L is **${mgFormatted} mg/dL** \n`);
 		}
-
-		if ( mg <= 900 ) {
-			message.channel.send(`${inputNum} mmol/L is **${mg} mg/dL** \n`);
+		
+		if ( mmolFormatted > 0.9 ) {
+			message.channel.send(`newline: ${inputNum} mg/dL is **${mmolFormatted} mmol/L** \n`);
 		}
 	},
 };
